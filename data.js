5a14f875c4b41b356aa1c2362b10b548715d7fc7
root <root@core-ssh.local.hass.io>
2021-09-13 15:20:35 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.3085125157683386,
+        "pitch": 0.01017612982431082,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05345303109143984,
+          "pitch": 0.3335161737412431,
+          "rotation": 6.283185307179586,
+          "target": "1-4"
+        },
+        {
+          "yaw": 0.010333682336945316,
+          "pitch": 0.05997347508926687,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.39096387807673594,
+          "pitch": 0.2969271376777698,
+          "title": "Colas Rail Egypt",
+          "text": "Planting Simulation"
+        }
+      ]
+    },
+    {
+      "id": "1-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.116219989963504,
+          "pitch": 0.3472021739070321,
+          "rotation": 0,
+          "target": "0-3"
+        },
+        {
+          "yaw": -0.043209388512822855,
+          "pitch": 0.13165964858763246,
+          "rotation": 12.566370614359176,
+          "target": "2-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.03992231222095,
+          "pitch": 0.2667100615654263,
+          "rotation": 0,
+          "target": "1-4"
+        },
+        {
+          "yaw": -3.036634142835833,
+          "pitch": 0.13226296451352582,
+          "rotation": 0,
+          "target": "0-3"
+        },
+        {
+          "yaw": 0.023560976373611098,
+          "pitch": 0.13175045200761204,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9212645861311195,
+          "pitch": 0.1832064243276612,
+          "rotation": 0,
+          "target": "4-7"
+        },
+        {
+          "yaw": -0.6320818129454082,
+          "pitch": 0.024588821305242092,
+          "rotation": 0,
+          "target": "5-8"
+        },
+        {
+          "yaw": 3.1321916420856724,
+          "pitch": 0.15605259699433205,
+          "rotation": 0,
+          "target": "2-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2397864305867614,
+          "pitch": 0.26008148417077237,
+          "rotation": 0,
+          "target": "5-8"
+        },
+        {
+          "yaw": 1.8208029974947335,
+          "pitch": 0.22888711170992693,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0215818072969274,
+          "pitch": 0.22331359473094103,
+          "rotation": 0,
+          "target": "4-7"
+        },
+        {
+          "yaw": 2.5710827383488697,
+          "pitch": 0.11109637124200766,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CRE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
